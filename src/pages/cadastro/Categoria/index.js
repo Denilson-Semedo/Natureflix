bdf83632e7a122ca3da1572244a62b9e8ea0ed12
Denilson Semedo <dstavares00@gmail.com>
2020-08-04 03:06:32 -0100

    Dando Continuidade com formulários

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, {/*useState*/} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+    /*const [categorias, setNomeDaCategoria] = useState(['Valor inicial']);
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');*/
+  
+
     return (
-        <PageDefault>
-            <h1>Categoria de Categoria</h1>
-            <Link to="/">
-                ir pra home
-            </Link>
-        </PageDefault>
+      <PageDefault>
+      <h1>Cadastro de Categoria: {/*nomeDaCategoria*/} </h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+            /*value = {nomeDaCategoria}
+            onChange = {function funcaoHanle(infoDoEvento){
+              setNomeDaCategoria(infoDoEvento.target.value)
+            }}*/
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+            <ul>
+              {/*categorias.map((categoria) => {
+                return (
+                  <li key = {categoria}>
+                    {categoria}
+                  </li>
+                )
+              })*/}
+            </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
     )      
 }
 
