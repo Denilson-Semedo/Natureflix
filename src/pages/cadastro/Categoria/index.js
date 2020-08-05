a15f4ff38abbcfaeea980f4a73a08427f5a7a1cc
Denilson Semedo <dstavares00@gmail.com>
2020-08-05 17:32:43 -0100

    Trabalhando com formulários

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,49 +1,79 @@
-import React, {/*useState*/} from 'react';
-import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import PageDefault from "../../../components/PageDefault";
+import { Link } from "react-router-dom";
 
 function CadastroCategoria() {
-    /*const [categorias, setNomeDaCategoria] = useState(['Valor inicial']);
-    const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');*/
-  
+  const [categorias, setCategorias] = useState(["Tester"]);
 
-    return (
-      <PageDefault>
-      <h1>Cadastro de Categoria: {/*nomeDaCategoria*/} </h1>
+  const valoresIniciais = {
+    nome: "Categoria Inicial",
+    descrição: "desc inicial",
+    cor: "#000",
+  };
 
-      <form>
+  const [values, setValues] = useState(valoresIniciais);
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-            /*value = {nomeDaCategoria}
-            onChange = {function funcaoHanle(infoDoEvento){
-              setNomeDaCategoria(infoDoEvento.target.value)
-            }}*/
-          />
-        </label>
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria: {values.nome} </h1>
 
-        <button>
-          Cadastrar
-        </button>
+      <form
+        onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+          setCategorias([...categorias, values]);
+        }}
+      >
+        <div>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+              value={values.nome}
+              onChange={function funcaoHanle(infosDoEvento) {
+                setValues(infosDoEvento.target.value);
+              }}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              onChange={function funcaoHanle(infosDoEvento) {
+                setValues(infosDoEvento.target.value);
+              }}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={values.cor}
+              onChange={function funcaoHanle(infosDoEvento) {
+                setValues(infosDoEvento.target.value);
+              }}
+            />
+          </label>
+        </div>
+
+        <button>Cadastrar</button>
       </form>
 
-            <ul>
-              {/*categorias.map((categoria) => {
-                return (
-                  <li key = {categoria}>
-                    {categoria}
-                  </li>
-                )
-              })*/}
-            </ul>
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return <li key={`${categoria}${indice}`}>{categoria}</li>;
+        })}
+      </ul>
 
-      <Link to="/">
-        Ir para home
-      </Link>
+      <Link to="/">Ir para home</Link>
     </PageDefault>
-    )      
+  );
 }
 
 export default CadastroCategoria;
